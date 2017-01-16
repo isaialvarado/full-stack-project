6427244ccfff9301a0b6709bb0554d5dbacbee77
Joel Alvarado <isaialvarado@users.noreply.github.com>
2017-01-16 10:15:00 -0800

    Add description and comments section to deal detail

--- frontend/components/deals/deal_detail.jsx
+++ frontend/components/deals/deal_detail.jsx
@@ -27,33 +27,40 @@ class DealDetail extends React.Component {
           <button
             onClick={() => this.props.deleteDeal(deal.id)
               .then(this.props.router.push('/'))}>
-              Delete Deal
+            Delete Deal
           </button>
         </div>
       );
     }
 
     return (
-      <section id='deal-detail-container'>
-        <div id='deal-detail'>
-          <div id='deal-detail-user-actions'>
-            {authorActions}
-            <div id='deal-detail-stats'>
-              <h2>thumbs</h2>
-              <h2>comments</h2>
+      <div>
+        <section id='deal-detail-top'>
+          <div id='deal-detail'>
+            <div id='deal-detail-user-actions'>
+              {authorActions}
+              <div id='deal-detail-stats'>
+                <h2>thumbs</h2>
+                <h2>comments</h2>
+              </div>
             </div>
+            <img src={deal.cloudUrl} />
+            <h1 id='deal-detail-title'>{deal.title}</h1>
+            <h2 id='deal-detail-price'>${deal.price} at {deal.vendor}</h2>
+            <a id='buy-now' href={deal.dealUrl}>Buy Now</a>
           </div>
-          <img src={deal.cloudUrl} />
-          <h1 id='deal-detail-title'>{deal.title}</h1>
-          <h2 id='deal-detail-price'>${deal.price} at {deal.vendor}</h2>
-          <a id='buy-now' href={deal.dealUrl}>Buy Now</a>
-          <div id='deal-detail-info'>
-            <h2>Description</h2>
-            <br />
-            <p>{deal.description}</p>
-          </div>
+        </section>
+        <div id='deal-detail-description'>
+          <h1>Description</h1>
+          <br />
+          <p>{deal.description}</p>
         </div>
-      </section>
+        <div id='deal-detail-comments'>
+          <h1>Comments</h1>
+          <br />
+          <p></p>
+        </div>
+      </div>
     );
   }
 }
